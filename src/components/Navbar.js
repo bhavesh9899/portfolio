6549aa8e708b17ecca3254220fa7040904c5a5c2
Bhavesh9899 <bhaveshvaghela9899@gmail.com>
2024-09-20 10:41:44 +0530

    new updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
             <nav className="navbar navbar-expand-md fixed-top ">
                 <div className="container-fluid">
                     <Link to="/" className="navbar-brand">
-                        <img src="logo.png" className="nav-logo" />
+                        <img src="logo.png" className="nav-logo" alt='logo'/>
                     </Link>
                     <button className="navbar-toggler" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar">
                         <RiBarChartHorizontalLine />
@@ -33,32 +33,17 @@ const Navbar = () => {
                                     <Link to="/projects" className="nav-link text-center">Project</Link>
                                 </li>
                                 <li className="nav-item dropdown">
-                                    <a className="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Social</a>
+                                    <a href="https://www.linkedin.com/in/bhavesh-vaghela-73577b238/" className="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">Social</a>
                                     <ul className="dropdown-menu" style={{ fontFamily: ' "Kanit", serif', fontWeight: '100' }}>
                                         <li>
-<<<<<<< HEAD
-                                            <a className="dropdown-item"
-                                                href="https://www.linkedin.com/in/bhavesh-vaghela-73577b238/" target='_bhavesh'><FaLinkedin className='text-center m-1 mx-2' style={{ color: '#0077b5' }} />Linkedin</a>
+                                            <a className="dropdown-item" href="https://www.linkedin.com/in/bhavesh-vaghela-73577b238/" target="_blank" rel="noreferrer"><FaLinkedin className='text-center m-1 mx-2' style={{ color: '#0077b5' }} />Linkedin</a>
+                                        </li>
+                                        <li>
+                                            <a className="dropdown-item" href="https://github.com/bhavesh9899" target='_blank' rel="noreferrer"><FaGithub className='text-center m-1 mx-2' style={{ color: '#000' }} />Github </a>
                                         </li>
                                         <li>
                                             <a className="dropdown-item"
-                                                href="https://github.com/bhavesh9899" target='_bhavesh'><FaGithub className='text-center m-1 mx-2' style={{ color: '#000' }} />Github </a>
-                                        </li>
-                                        <li>
-                                            <a className="dropdown-item"
-                                                href="https://wa.me/+919979258370/?text=Hello" target='_bhavesh'><IoLogoWhatsapp className='text-center m-1 mx-2' style={{ color: 'green' }} />Whatsapp</a>
-=======
-                                            <a className="dropdown-item text-center"
-                                                href="https://instragram.com/mr._b_vaghela" target="_bhavesh"><FaInstagram className='text-center m-1' style={{ color: '#dd2a7b' }} />Instagram</a>
-                                        </li>
-                                        <li>
-                                            <a className="dropdown-item text-center"
-                                                href="https://www.facebook.com/share/Y5ACUZhGKUj8SZKD/?mibextid=qi20mg" target="_bhavesh"><FaFacebookSquare className='text-center m-1' style={{ color: 'rgb(69, 116, 226)' }} />Facebook</a>
-                                        </li>
-                                        <li>
-                                            <a className="dropdown-item text-center"
-                                                href="https://wa.me/+919979258370/?text=Hello" target="_bhavesh"><FaWhatsapp className='text-center m-1' style={{ color: 'green' }} />Whatsapp</a>
->>>>>>> 2e3eb082e7c66cf9c94b32abfb969bf0ee463974
+                                                href="https://wa.me/+919979258370/?text=Hello" target='_blank' rel="noreferrer"><IoLogoWhatsapp className='text-center m-1 mx-2' style={{ color: 'green' }} />Whatsapp</a>
                                         </li>
                                     </ul>
                                 </li>
