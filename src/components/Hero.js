e941b5ce75c7a653582970e2c718020b2d69bbe9
bhavesh <bhaveshvaghela9899@gmail.com>
2026-08-29 10:03:55 +0530

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { FaInstagram, FaWhatsapp, FaFacebookSquare } from 'react-icons/fa';
 import './Hero.css';
 
